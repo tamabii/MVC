444155b9ea9797ecc07962476e7ab45d6c1bafe2
tamabii <107445347+tamabii@users.noreply.github.com>
2023-09-29 15:53:00 +0700

    Create contoh1.php

---
+++ contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "nama saya Hernando Khidam hobi saya basket";
+    }
+}
